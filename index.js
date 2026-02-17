5f1e31e08d0d38ec64817447f5525b214da4aee8
jajajawwswws-lab <jajajawwswws@gmail.com>
2026-02-17 18:17:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 formData.append('g-recaptcha-response', token);
 
                 // ✅ CORREÇÃO: Apontar para o servidor Node.js
-                const respostaBackend = await fetch("back2.js", {
+                const respostaBackend = await fetch("https://localhost:3000/login", {
                     method: "POST",
                     body: formData
                 }).then(result => result.json());
