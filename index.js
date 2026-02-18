620235c77e451d4e8d0800cd3dc561472c58ee8b
jajajawwswws-lab <jajajawwswws@gmail.com>
2026-02-17 21:34:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function enviarFormulario(event) {
         }
 
         // CHAMAR API
-        const response = await fetch('/api/backend', {
+        const response = await fetch('api/backend', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
