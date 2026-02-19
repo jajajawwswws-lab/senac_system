b0a0ae79edd46a7e137c5a315ec468e98090b413
jajajawwswws-lab <jajajawwswws@gmail.com>
2026-02-19 17:27:48 -0500

    Create create_account.ts

---
+++ api/create_account.ts
@@ -0,0 +1,19 @@
+interface Registro{
+    id: number;
+    username: string;
+    phone: number;
+    email: string;
+}
+console.log("working...");
+//import express, {Express, Request, Response} from "express";
+//import session from "express-session";
+//const express = require('express');
+//const session = require('express-session');
+//const fetch = require('node-fetch');
+//const cors = require('cors');
+
+//const app = express();
+//const port = 3000;
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
